9ab4e73b8c68299d6d9b40169fbe8cf5db604e84
Metin Alptekin <metinalptekin10@gmail.com>
2025-08-27 10:42:52 +0300

    Update proje.ino

--- 16_flex_sensör_ile_eğilme_algılama/proje.ino
+++ 16_flex_sensör_ile_eğilme_algılama/proje.ino
@@ -1,11 +1,23 @@
-// Flex Sensör ile Eğilme Algılama
-// Başlangıç iskeleti — giriş/çıkış pinlerini ve temel döngüyü yapılandırın.
+// C++ code
+//
+int sensor = 0;
 
-void setup() {{
-  // pinMode(...);
-  // Serial.begin(9600);
-}}
+void setup()
+{
+  pinMode(A0, INPUT);
+  pinMode(8, OUTPUT);
+  Serial.begin(9600);
+}
 
-void loop() {{
-  // TODO: Sensör verisini oku, işle ve çıktıyı ver.
-}}
+void loop()
+{
+  sensor = analogRead(A0);
+  if (sensor < 20) {
+    digitalWrite(8, HIGH);
+  } else {
+    digitalWrite(8, LOW);
+  }
+  Serial.print("sensor = ");
+  Serial.println(sensor);
+  delay(100); // Wait for 100 millisecond(s)
+}
