883bbc723b4a90a8995a5e4ae05ce71683d6c915
Metin Alptekin <metinalptekin10@gmail.com>
2025-08-27 11:27:58 +0300

    Update proje.ino

--- 21_2_eksenli_joystick_ile_servo_kontrol/proje.ino
+++ 21_2_eksenli_joystick_ile_servo_kontrol/proje.ino
@@ -1,11 +1,22 @@
-// 2 Eksenli Joystick ile Servo Kontrol
-// Başlangıç iskeleti — giriş/çıkış pinlerini ve temel döngüyü yapılandırın.
-
-void setup() {{
-  // pinMode(...);
-  // Serial.begin(9600);
-}}
-
-void loop() {{
-  // TODO: Sensör verisini oku, işle ve çıktıyı ver.
-}}
+#include <Servo.h>
+int xPin = A0; 
+int yPin = A1; 
+Servo motor1; Servo motor2;
+ 
+int xPozisyon = 0;
+int yPozisyon = 0;
+ 
+ 
+void setup() {
+pinMode(xPin, INPUT);
+pinMode(yPin, INPUT);
+motor1.attach(5);
+motor2.attach(6);
+}
+ 
+void loop() {
+xPozisyon = map(analogRead(xPin),0,1023,0,180);
+yPozisyon = map(analogRead(yPin),0,1023,0,180);
+motor1.write(xPozisyon);
+motor2.write(yPozisyon);
+}
