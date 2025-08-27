0d12dfe94311a6b7356cd01e1c4fa6121775f4d1
Metin Alptekin <metinalptekin10@gmail.com>
2025-08-27 10:35:50 +0300

    Update proje.ino

--- 15_joystick_ile_led_kontrolü/proje.ino
+++ 15_joystick_ile_led_kontrolü/proje.ino
@@ -1,11 +1,23 @@
-// Joystick ile LED Kontrolü
-// Başlangıç iskeleti — giriş/çıkış pinlerini ve temel döngüyü yapılandırın.
-
-void setup() {{
-  // pinMode(...);
-  // Serial.begin(9600);
-}}
-
-void loop() {{
-  // TODO: Sensör verisini oku, işle ve çıktıyı ver.
-}}
+int xPin = A0; 
+int yPin = A1; 
+int ledK=2; int ledM=3; int ledY=4; int ledS=5;
+ 
+int xPozisyon = 0;
+int yPozisyon = 0;
+ 
+ 
+void setup() {
+pinMode(xPin, INPUT);
+pinMode(yPin, INPUT);
+pinMode(ledK,OUTPUT); pinMode(ledM,OUTPUT); 
+pinMode(ledY,OUTPUT); pinMode(ledS,OUTPUT);
+}
+ 
+void loop() {
+xPozisyon = analogRead(xPin);
+yPozisyon = analogRead(yPin);
+if(xPozisyon<10){digitalWrite(ledK,HIGH);}else{digitalWrite(ledK,LOW);}
+if(xPozisyon>1000){digitalWrite(ledM,HIGH);}else{digitalWrite(ledM,LOW);} 
+if(yPozisyon<10){digitalWrite(ledY,HIGH);}else{digitalWrite(ledY,LOW);}
+if(yPozisyon>1000){digitalWrite(ledS,HIGH);}else{digitalWrite(ledS,LOW);} 
+}
