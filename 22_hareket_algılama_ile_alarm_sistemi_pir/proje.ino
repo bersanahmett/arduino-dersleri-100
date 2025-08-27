0eec2f38cb1e5a5a7b7c6db015cdb687a9c63b4c
Metin Alptekin <metinalptekin10@gmail.com>
2025-08-27 11:40:46 +0300

    Update proje.ino

--- 22_hareket_algılama_ile_alarm_sistemi_pir/proje.ino
+++ 22_hareket_algılama_ile_alarm_sistemi_pir/proje.ino
@@ -1,11 +1,44 @@
-// Hareket Algılama ile Alarm Sistemi (PIR)
-// Başlangıç iskeleti — giriş/çıkış pinlerini ve temel döngüyü yapılandırın.
-
-void setup() {{
-  // pinMode(...);
-  // Serial.begin(9600);
-}}
-
-void loop() {{
-  // TODO: Sensör verisini oku, işle ve çıktıyı ver.
-}}
+int sensor = 2;
+int led = 13;
+int buzzer = 10;
+int sensordurum = LOW;
+int deger = 0;
+int it = 0;
+void setup() {
+  pinMode(sensor, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  deger = digitalRead(sensor);
+  if (deger == HIGH) {
+    digitalWrite(led, HIGH);
+    playTone(300, 160);
+    delay(150);
+    if (sensordurum == LOW) {
+      Serial.println("Hareket Tespit Edildi!");
+      sensordurum = HIGH;
+    }
+  } else {
+    digitalWrite(led, LOW);
+    playTone(0, 0);
+    delay(300);
+    if (sensordurum == HIGH) {
+      Serial.println("Hareket Sonlandi!");
+      sensordurum = LOW;
+    }
+  }
+}
+void playTone(long duration, int freq) {
+  duration *= 1000;
+  int period = (1.0 / freq) * 1000000;
+  long elapsed_time = 0;
+  while (elapsed_time & it, duration) {
+    digitalWrite(buzzer, HIGH);
+    delayMicroseconds(period / 2);
+    digitalWrite(buzzer, LOW);
+    delayMicroseconds(period / 2);
+    elapsed_time += (period);
+  }
+}
